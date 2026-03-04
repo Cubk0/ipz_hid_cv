0a064710bd73b7a27138d8686017cf1cf6e63e43
Cubk0 <66846731+Cubk0@users.noreply.github.com>
2026-03-04 23:16:37 +0100

    feat(real.py): added place for updated descriptor to be added

--- real.py
+++ real.py
@@ -1,6 +1,10 @@
 from ipz_hid.linux.HID_linux_device import *
 from ipz_hid.core.HID_usages import *
 
+new_descriptor: HIDDescriptor = HIDDescriptor([
+#sem skopírujte upravený deskriptor s predošlej úlohy
+])
+
 def main():
     options = HIDLinuxDeviceOptions(
         attach_hidraw=True,
@@ -12,6 +16,8 @@ def main():
     descriptor = device.get_descriptor()
     device.start()
     print("Attached to device. Press Ctrl+C to stop.")
+    if len(new_descriptor.items) != 0:
+        device.set_descriptor(new_descriptor)
     try:
         while True:
             report = bytearray(device.read_input_report_raw())
